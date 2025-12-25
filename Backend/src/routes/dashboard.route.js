96b4cb60dbdfadfdcec51bc663ad6c3baa59d448
Vaishnavi Bharanale <vaishnavibharanale@gmail.com>
2025-12-25 12:28:26 +0530

    Delete Backend directory

--- Backend/src/routes/dashboard.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getDashboardData } from "../controllers/dashboard.controller.js";
-import { verifyToken } from "../middleware/auth.middleware.js";
-const router = express.Router();
-// This route matches your frontend URL: /api/dashboard/:userId
-router.get("/", verifyToken, getDashboardData);
-
-export default router;
