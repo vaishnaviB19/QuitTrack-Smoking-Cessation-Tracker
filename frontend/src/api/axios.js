a799b172f2190c615db61769d33c976892061907
Vaishnavi Bharanale <vaishnavibharanale@gmail.com>
2026-01-20 14:27:57 +0530

    Fixed axios URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
-  // baseURL: import.meta.env.VITE_API_URL,
+  // baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true
 });
 
